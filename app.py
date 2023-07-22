ad026b1aaa10f0a39a7889b2c8e4454481fedc17
akhialomgir <akhialomgir362856@gmail.com>
2023-07-22 11:22:23 +0800

    Add login

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response, session
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources=r"/*")
 
 
-@app.route("/checkout", methods=["GET"])
+@app.route("/api/checkout", methods=["GET"])
 def checkout():
-    return jsonify({"msg": "success"}), 200
+    return Response({"msg": "success"}), 200
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    resp = Response()
+    resp.headers["Access-Control-Allow-Origin"] = "*"
+    user = request.form.get("user")
+    pwd = request.form.get("password")
+    if user == "123@qq.com" and pwd == "123":
+        session["user"] = user
+        return resp, 200
+    else:
+        return resp, 401
 
 
 if __name__ == "__main__":
